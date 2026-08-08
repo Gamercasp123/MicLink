44ec6d9e4ba545ec6d83cbead31400190feb3c21
Zsolt <zsoltsabo85@gmail.com>
2026-08-08 21:35:38 +0100

    Use Expo registerRootComponent and clean app.json

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import 'react-native-gesture-handler';
-import { AppRegistry } from 'react-native';
+import { registerRootComponent } from 'expo';
 import { Buffer } from 'buffer';
 import App from './App';
-import { name as appName } from './app.json';
 
 global.Buffer = Buffer;
 
-AppRegistry.registerComponent(appName, () => App);
+registerRootComponent(App);
